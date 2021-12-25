d64750555d331f790828bf1a21b174eb8b2735cc
apple <apple@iMac-apple.local>
2021-12-25 03:47:11 -0800

    video

--- about/admin.py
+++ about/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
+
 from about.models import AboutModel
 
 
 @admin.register(AboutModel)
 class AboutModelAdmin(admin.ModelAdmin):
-    list_display = ['title', 'created_at']
+    list_display = ['title', 'created_at', 'video', ]
     search_fields = ['title']
     list_filter = ['title', 'created_at']
+
+
--- about/models.py
+++ about/models.py
@@ -5,6 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class AboutModel(models.Model):
     title = models.CharField(max_length=90, verbose_name=_('title'))
     image = models.ImageField(upload_to='about', verbose_name=_('image'))
+    video = models.FileField(upload_to='about_video', verbose_name=_('video'), null=True)
     description = models.TextField(verbose_name=_('description'))
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created_at'))
 
