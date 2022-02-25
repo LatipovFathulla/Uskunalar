1af3f54824e1edd54bba029c989b803ef56b696e
apple <apple@iMac-apple.local>
2022-02-25 03:48:12 -0800

    abouts

--- about/models.py
+++ about/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from embed_video.fields import EmbedVideoField
 
 
 class AboutModel(models.Model):
     title = models.CharField(max_length=90, verbose_name=_('title'))
     image = models.ImageField(upload_to='about', verbose_name=_('image'))
     description = models.TextField(verbose_name=_('description'))
+    url = EmbedVideoField(null=True)
+    long_description = models.TextField(verbose_name=_('long_description'), null=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created_at'))
 
     def __str__(self):
