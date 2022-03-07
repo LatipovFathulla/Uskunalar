e7062d37b22ce72f38fd7301ec7f8842c022b425
apple <apple@iMac-apple.local>
2022-03-07 03:37:46 -0800

    lonrse

--- blog/templatetags/tags.py
+++ blog/templatetags/tags.py
@@ -10,6 +10,11 @@ def get_url_lang(request, lang):
     return '/'.join(url)
 
 
+@register.filter()
+def in_wishlist(wishlist, request):
+    return wishlist.pk in request.session.get('wishlist', [])
+
+
 @register.simple_tag
 def get_wishlist_count(request):
     wishlist = request.session.get('wishlist')
