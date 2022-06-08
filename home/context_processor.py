a8bce7eaa09588f5da39e965386a4e2e726aea6d
apple <apple@iMac-apple.local>
2022-06-08 06:31:40 -0700

    catalog

--- home/context_processor.py
+++ home/context_processor.py
@@ -6,12 +6,12 @@ def product_categories(request):
     categories = cache.get('categories')
     if not categories:
         categories = CategoryModel.objects.order_by('pk')
-        cache.set('categories', categories, 60)
+        cache.set('categories', categories, 10)
 
     subcategories = cache.get('subcategories')
     if not subcategories:
         subcategories = SubCategoryModel.objects.order_by('pk')
-        cache.set('subcategories', subcategories, 60)
+        cache.set('subcategories', subcategories, 10)
 
     return {
         'categories': categories,
