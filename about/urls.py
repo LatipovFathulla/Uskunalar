c84775b2bbb3c4db178778145981dddea18b49e2
apple <apple@iMac-apple.local>
2022-06-23 05:18:56 -0700

    vbby

--- about/urls.py
+++ about/urls.py
@@ -7,12 +7,12 @@ from orders.views import OrderCreateView
 app_name = 'home'
 
 urlpatterns = [
-    path('about/', cache_page(1800)(AboutModelListView.as_view()), name='about'),
+    path('about/', cache_page(1)(AboutModelListView.as_view()), name='about'),
     path('<int:pk>/', AboutModelDetailView.as_view(), name='detail'),
     path('contacts/', ContactModelTemplateView.as_view(), name='contacts'),
     path('catalog/', CatalogView.as_view(), name='catalog'),
     path('request/', RequestCreateView.as_view(), name='request'),
     path('form/', OrderCreateView.as_view(), name='form'),
-    path('', cache_page(1800)(HomeView.as_view()), name='home'),
+    path('', cache_page(1)(HomeView.as_view()), name='home'),
 
 ]
