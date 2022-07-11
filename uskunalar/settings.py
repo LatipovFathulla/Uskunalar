424080ebec796a763b6123ac847c20e05b86cc13
apple <apple@iMac-apple.local>
2022-07-11 05:53:54 -0700

    edit times

--- uskunalar/settings.py
+++ uskunalar/settings.py
@@ -98,12 +98,12 @@ DATABASES = {
     }
 }
 #
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-#         'LOCATION': BASE_DIR / 'uskunalar_cache',
-#     }
-# }
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': BASE_DIR / 'uskunalar_cache',
+    }
+}
 # cache
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
