71dc7c361e0934485758bd2550024ff95ae8eb3c
apple <apple@iMac-apple.local>
2022-07-20 04:00:12 -0700

    trt

--- biznes/urls.py
+++ biznes/urls.py
@@ -5,6 +5,6 @@ from biznes.views import BiznesModelView, BiznesModelDetailView
 app_name = 'biznes'
 
 urlpatterns = [
-    path('', cache_page(1800)(BiznesModelView.as_view()), name='biznes'),
-    path('<int:pk>/', cache_page(1800)(BiznesModelDetailView.as_view()), name='detail')
+    path('', cache_page(1)(BiznesModelView.as_view()), name='biznes'),
+    path('<int:pk>/', cache_page(1)(BiznesModelDetailView.as_view()), name='detail')
 ]
