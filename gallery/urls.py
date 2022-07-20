6e5331824a32781ce1767dcb9b2b55db19dda61e
apple <apple@iMac-apple.local>
2022-07-20 03:41:18 -0700

    end cache

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
+from django.views.decorators.cache import cache_page
 from gallery.views import GalleryTemplateView, SingleGalleryDetailView
 
 app_name = 'galleries'
 
 urlpatterns = [
-    path('<int:pk>/', SingleGalleryDetailView.as_view(), name='single'),
-    path('', GalleryTemplateView.as_view(), name='gallery')
+    path('<int:pk>/', cache_page(1800)(SingleGalleryDetailView.as_view()), name='single'),
+    path('', cache_page(1800)(GalleryTemplateView.as_view()), name='gallery')
 ]
