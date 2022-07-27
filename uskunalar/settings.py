70081cda2c328497ccd6f2a78da37d8f15f8a2b7
apple <apple@iMac-apple.local>
2022-07-27 05:33:56 -0700

    new api

--- uskunalar/settings.py
+++ uskunalar/settings.py
@@ -185,7 +185,7 @@ DEBUG_TOOLBAR_CONFIG = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 9
