5e362e46bafa0c81ef79d4da991c7fb800dc9791
User <user@MacBook-Air-User.local>
2022-09-14 20:11:25 +0500

    change biznes models

--- biznes/models.py
+++ biznes/models.py
@@ -7,7 +7,7 @@ from django.utils.translation import gettext_lazy as _
 class BiznesModel(models.Model):
     title = models.CharField(max_length=250, verbose_name=_('title'), null=True, blank=True)
     image = models.FileField(upload_to='biznes', verbose_name=_('biznes_image'))
-    long_descriptions = RichTextUploadingField(verbose_name=_('long_descriptions'))
+    long_descriptions = models.TextField(verbose_name=_('long_descriptions'))
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created_at'))
 
     def get_prev(self):
@@ -25,10 +25,10 @@ class BiznesModel(models.Model):
 
 
 class BiznesImageModel(models.Model):
-    biznes_image = models.ForeignKey(BiznesModel, on_delete=models.CASCADE, related_name='biznes_image', verbose_name=_('biznes_image'), null=True)
-    biznes_spec = models.CharField(max_length=200, verbose_name=_('biznes_spec'), null=True)
-    biznes_num = models.CharField(max_length=200, verbose_name=_('biznes_num'), null=True)
-    image = models.FileField(upload_to='biznes_single', verbose_name=_('biznes_image'), null=True)
+    biznes_image = models.ForeignKey(BiznesModel, on_delete=models.CASCADE, related_name='biznes_image', verbose_name=_('biznes_image'), null=True, blank=True)
+    biznes_spec = models.CharField(max_length=200, verbose_name=_('biznes_spec'), null=True, blank=True)
+    biznes_num = models.CharField(max_length=200, verbose_name=_('biznes_num'), null=True, blank=True)
+    image = models.FileField(upload_to='biznes_single', verbose_name=_('biznes_image'), null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created_at'), null=True)
 
     class Meta:
