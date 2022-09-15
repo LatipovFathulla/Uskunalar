7cdb1030411be5ed9367b6c4c5bc1717357a5932
User <user@MacBook-Air-User.local>
2022-09-16 00:22:16 +0500

    server

--- uskunalar/settings.py
+++ uskunalar/settings.py
@@ -250,7 +250,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:4200',
     'http://127.0.0.1',
-    'http://5.23.52.211'
+    'http://134.209.239.110'
 )
 try:
     from .settings_local import *
