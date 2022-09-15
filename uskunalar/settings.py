1476d114f65c612faf0127fa69b8dbfb8fa3ed57
User <user@MacBook-Air-User.local>
2022-09-16 00:24:59 +0500

    ser

--- uskunalar/settings.py
+++ uskunalar/settings.py
@@ -248,8 +248,6 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:4200',
-    'http://127.0.0.1',
     'http://134.209.239.110'
 )
 try:
