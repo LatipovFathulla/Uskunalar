bb173e910f56649861d9f1f749a43ce95d56a47e
User <user@MacBook-Air-User.local>
2022-11-04 17:52:39 +0500

    new api fields for product3

--- uskunalar/settings.py
+++ uskunalar/settings.py
@@ -236,7 +236,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 9
+    'PAGE_SIZE': 16
 }
 
 AUTHENTICATION_BACKENDS = (
