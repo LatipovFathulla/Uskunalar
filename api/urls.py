7edd48a1b383dddb8c8e272b02b29782cf740e15
User <user@MacBook-Air-User.local>
2022-11-09 20:34:53 +0500

    detail api

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,26 @@
 from api.views import BannerInfoModelAPIView, LinesInfoModelAPIView, BiznesInfoModelAPIView, VideoInfoModelAPIView, \
-    BlogInfoModelAPIView, AboutInfoModelAPIView, GalleryInfoModelAPIView, WorkInfoModelAPIView
+    BlogInfoModelAPIView, AboutInfoModelAPIView, GalleryInfoModelAPIView, WorkInfoModelAPIView, BannerDetailAPIView, \
+    LinesDetailAPIView, BiznesDetailAPIView, VideoDetailAPIView, BlogDetailAPIView, AboutDetailAPIView, \
+    GalleryDetailAPIView, WorkDetailAPIView
 from django.urls import path
 
 app_name = 'api'
 
 urlpatterns = [
     path('products/', BannerInfoModelAPIView.as_view(), name='products'),
+    path('products/<int:pk>', BannerDetailAPIView.as_view(), name='products-detail'),
     path('lines/', LinesInfoModelAPIView.as_view(), name='lines'),
+    path('lines/<int:pk>', LinesDetailAPIView.as_view(), name='lines-detail'),
     path('biznes/', BiznesInfoModelAPIView.as_view(), name='biznes'),
+    path('biznes/<int:pk>', BiznesDetailAPIView.as_view(), name='biznes-detail'),
     path('videos/', VideoInfoModelAPIView.as_view(), name='videos'),
+    path('videos/<int:pk>', VideoDetailAPIView.as_view(), name='videos-detail'),
     path('blogs/', BlogInfoModelAPIView.as_view(), name='blogs'),
+    path('blogs/<int:pk>', BlogDetailAPIView.as_view(), name='blogs-detail'),
     path('abouts/', AboutInfoModelAPIView.as_view(), name='abouts'),
+    path('abouts/<int:pk>', AboutDetailAPIView.as_view(), name='abouts-detail'),
     path('galleries/', GalleryInfoModelAPIView.as_view(), name='galleries'),
+    path('galleries/<int:pk>', GalleryDetailAPIView.as_view(), name='galleries-detail'),
     path('works/', WorkInfoModelAPIView.as_view(), name='works'),
+    path('works/<int:pk>', WorkDetailAPIView.as_view(), name='works-detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from about.models import AboutModel
 from api.serializer import BannerInfoModelSerializer, LinesModelSerializer, BiznesModelSerializer, VideoModelSerializer, \
@@ -18,9 +20,23 @@ class BannerInfoModelAPIView(ListAPIView):
     serializer_class = BannerInfoModelSerializer
 
 
+class BannerDetailAPIView(APIView):
+    def get(self, request, pk):
+        products = BannerInfoModel.objects.get(pk=pk)
+        serializer = BannerInfoModelSerializer(products, context={'request': request})
+        return Response(serializer.data)
+
+
 class LinesInfoModelAPIView(ListAPIView):
     queryset = LineModel.objects.all()
     serializer_class = LinesModelSerializer
+
+
+class LinesDetailAPIView(APIView):
+    def get(self, request, pk):
+        products = LineModel.objects.get(pk=pk)
+        serializer = LinesModelSerializer(products, context={'request': request})
+        return Response(serializer.data)
 
 
 class BiznesInfoModelAPIView(ListAPIView):
@@ -28,9 +44,23 @@ class BiznesInfoModelAPIView(ListAPIView):
     serializer_class = BiznesModelSerializer
 
 
+class BiznesDetailAPIView(APIView):
+    def get(self, request, pk):
+        products = BiznesModel.objects.get(pk=pk)
+        serializer = BiznesModelSerializer(products, context={'request': request})
+        return Response(serializer.data)
+
+
 class VideoInfoModelAPIView(ListAPIView):
     queryset = VideoModel.objects.all()
     serializer_class = VideoModelSerializer
+
+
+class VideoDetailAPIView(APIView):
+    def get(self, request, pk):
+        products = VideoModel.objects.get(pk=pk)
+        serializer = VideoModelSerializer(products, context={'request': request})
+        return Response(serializer.data)
 
 
 class BlogInfoModelAPIView(ListAPIView):
@@ -38,9 +68,23 @@ class BlogInfoModelAPIView(ListAPIView):
     serializer_class = BlogModelSerializer
 
 
+class BlogDetailAPIView(APIView):
+    def get(self, request, pk):
+        products = BlogModel.objects.get(pk=pk)
+        serializer = BlogModelSerializer(products, context={'request': request})
+        return Response(serializer.data)
+
+
 class AboutInfoModelAPIView(ListAPIView):
     queryset = AboutModel.objects.all()
     serializer_class = AboutModelSerializer
+
+
+class AboutDetailAPIView(APIView):
+    def get(self, request, pk):
+        products = AboutModel.objects.get(pk=pk)
+        serializer = AboutModelSerializer(products, context={'request': request})
+        return Response(serializer.data)
 
 
 class GalleryInfoModelAPIView(ListAPIView):
@@ -48,6 +92,20 @@ class GalleryInfoModelAPIView(ListAPIView):
     serializer_class = GalleryModelSerializer
 
 
+class GalleryDetailAPIView(APIView):
+    def get(self, request, pk):
+        products = GalleryModel.objects.get(pk=pk)
+        serializer = GalleryModelSerializer(products, context={'request': request})
+        return Response(serializer.data)
+
+
 class WorkInfoModelAPIView(ListAPIView):
     queryset = WorkModel.objects.all()
     serializer_class = WorkModelSerializer
+
+
+class WorkDetailAPIView(APIView):
+    def get(self, request, pk):
+        products = WorkModel.objects.get(pk=pk)
+        serializer = WorkModelSerializer(products, context={'request': request})
+        return Response(serializer.data)
