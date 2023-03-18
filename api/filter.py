0494383def60e85005c5a5ef43f499fc3e7bf067
FathullaLatipov <fathullaxonlatipov@gmail.com>
2023-03-18 15:30:44 +0500

    pr

--- api/filter.py
+++ api/filter.py
@@ -1,12 +1,15 @@
 import django_filters
 
-from home.models import BannerInfoModel
+from home.models import BannerInfoModel, CategoryModel, SubCategoryModel
 
 
 class ProductFilter(django_filters.FilterSet):
+    category = django_filters.ModelChoiceFilter(field_name='category', queryset=CategoryModel.objects.all())
+    subcategory = django_filters.ModelChoiceFilter(field_name='subcategory', queryset=SubCategoryModel.objects.all())
+    view_count = django_filters.NumberFilter(field_name='view_count',)
     min_price = django_filters.NumberFilter(field_name='price', lookup_expr='gte')
     max_price = django_filters.NumberFilter(field_name='price', lookup_expr='lte')
 
     class Meta:
         model = BannerInfoModel
-        fields = ['price']
+        fields = ['category', 'subcategory', 'view_count', 'price']
